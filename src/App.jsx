c5f886dca4038310440d986e99cb3fac8cb58062
Arthur Jorge <arthur.sjorge@gmail.com>
2025-12-18 17:29:09 +0100

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Shop from './pages/Shop'
 import { Routes, Route } from "react-router-dom";
+import Footer from './components/Footer';
+import Cart from './pages/Cart';
+import AddItem from './pages/AddItem';
+import ItemDetails from './pages/ItemDetails';
+import EditItem from './pages/EditItem';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,8 +19,14 @@ function App() {
         <h1>Hallo</h1>
         <Routes>
           <Route path='/' element={<Shop/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/add-item' element={<AddItem/>} />
+          <Route path='/item-details' element={<ItemDetails/>} />
+          <Route path='/edit-item' element={<EditItem/>} />
 
         </Routes>
+
+        <Footer/>
 
       </div>
       <p className="read-the-docs">
