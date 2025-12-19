624359b821a94dbd1faace53c87ae54e1c83232b
Arthur Jorge <arthur.sjorge@gmail.com>
2025-12-19 17:18:31 +0100

    merge problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/common/Navbar";
 import Shop from "./pages/Shop";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
